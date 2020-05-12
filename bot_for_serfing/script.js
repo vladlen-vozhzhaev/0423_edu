cb2025b9d2db761350952f1cb9898a57d48d4cdc
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-12 21:05:35 +0300

    Продолжаем писать бота 12.05.2020
    1) Создали массив ключевых слов
    2) Добавили функцию getRandom();
    3) Выбираем ключевое слово случайным образом из массива keywords
    4) Добавили случайные задержки времени перед кликами (от 3 секунд до 10 секунд)

--- bot_for_serfing/script.js
+++ bot_for_serfing/script.js
@@ -3,21 +3,29 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       https://vozhzhaev.ru
+// @author       You
 // @match        https://www.google.com/*
 // @grant        none
 // ==/UserScript==
 
 let btnK = document.getElementsByName('btnK')[1];
+let keywords = ["Гобой", "Как звучит флейта", "Что такое валторна", "Как выглядит тромбон", "Музыкальные диктанты онлайн", "Виолончель"];
+let keyword  = keywords[getRandom(0,keywords.length)];
 
 if (btnK!=undefined){
-    document.getElementsByName('q')[0].value = "Гобой";
-    btnK.click()
+    document.getElementsByName('q')[0].value = keyword;
+    setTimeout(()=>btnK.click(), getRandom(3000,10000));
 }
+
 let links = document.links;
 for(let i=0; i<links.length; i++){
     if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1){
-        links[i].click();
+        let link = links[i];
+        setTimeout(()=>link.click(), getRandom(3000,10000));
         break;
     }
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
 }
