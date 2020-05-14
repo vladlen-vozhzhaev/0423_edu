6e615e3463c249690e06fd4f99fdc9762d559dd1
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-14 20:51:58 +0300

    Продолжаем писать бота 14.05.2020
    1) Добавили объект sites в котором содержится список сайтов в качестве ключей и ключевые слова в качестве значений.
    2) Реализовали установку cookie для сохранения сайта по которому нужно кликнуть на странице поисковой выдачи.

--- bot_for_serfing/script.js
+++ bot_for_serfing/script.js
@@ -6,27 +6,36 @@
 // @author       You
 // @match        https://www.google.com/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
 
-let btnK = document.getElementsByName('btnK')[1];
-let keywords = ["Гобой", "Как звучит флейта", "Что такое валторна", "Как выглядит тромбон", "Музыкальные диктанты онлайн", "Виолончель"];
-let keyword  = keywords[getRandom(0,keywords.length)];
-
-if (btnK!=undefined){
-    writeWord(keyword);
-}
 let links = document.links;
-
+let site = location.host;
 if (location.host == "www.google.com"){
+    let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой", "Как звучит флейта", "Что такое валторна", "Как выглядит тромбон", "Музыкальные диктанты онлайн", "Виолончель"],
+    "crushdrummers.ru":["Барабанное шоу","Шоу барабанщиков москва","Заказать барабанное шоу"]
+    }
+    var btnK = document.getElementsByName('btnK')[1];
+    if (btnK!=undefined){
+        site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+        let keywords = sites[site];
+        let keyword  = keywords[getRandom(0,keywords.length)];
+        document.cookie = "site="+site;
+        writeWord(keyword);
+    }
+    else {
+        site = getCookie('site');
+    }
     getGooglePage();
 }
 else {
     if (getRandom(0,100)>20){
         let index = getRandom(0,links.length);
-        if(links[index].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1)
+        if(links[index].href.indexOf(site)!=-1)
             setTimeout(()=>{links[index].click();},getRandom(3000, 10000));
-        else location.href = 'https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/';
+        else location.href = `https://${site}/`;
     }
     else location.href = "https://www.google.com/";
 }
@@ -50,8 +59,9 @@ function writeWord(keyword){
 function getGooglePage(){
     let goNextPage = true;
     for(let i=0; i<links.length; i++){
-        if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1){
+        if(links[i].href.indexOf(site)!=-1){
             let link = links[i];
+            setTimeout(()=>{link.scrollIntoView(true);},2000);
             goNextPage = false;
             setTimeout(()=>link.click(), getRandom(3000,10000));
             break;
@@ -62,3 +72,10 @@ function getGooglePage(){
         else pnnext.click();
     }, getRandom(3000,10000));
 }
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+ }
